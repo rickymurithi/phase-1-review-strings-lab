3bce2558f3377defde6f267e46da31c2e182d457
erickmuriithi <erick.muriithi@student.moringaschool.com>
2022-08-19 09:04:06 +0300

    add file

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Ricky Murithi';
+//const welcomeMessage = 'Welcome to Flatbook, Ricky Murithi!';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+currentUser.slice(0, 1);
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
